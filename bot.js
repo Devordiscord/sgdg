cc77563fc48f21090a22ae57150d39c9e66c9678
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-02-08 22:54:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1501,7 +1501,7 @@ if(message.content.startsWith(prefix + "daily")) {
   if(profile[message.author.id].lastDaily != moment().format('day')) {
     profile[message.author.id].lastDaily = moment().format('day')
     profile[message.author.id].credits += 200
-     message.channel.send(`**\`200\` :dollar: daily pounds**`)
+     message.channel.send(`** \`\`200\`\` :dollar: daily pounds**`)
 } else {
     message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
 }
