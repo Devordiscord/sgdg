30e191c6e6b44c00bd7be029e4b7d7092faeb591
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-02-08 23:04:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1542,4 +1542,114 @@ message.channel.send(`${msg.author} has tranfered \`$${args[1]}\` to ${men}.`)
 
 });
 
+client.on('message', message => {
+if (message.content.startsWith("t!profile")) { // الامر
+    let getvalueof = msg.mentions.users.first() || msg.author;
+
+    let res = await SQLite.get(`SELECT * FROM profileSystem WHERE id = ${getvalueof.id}`)
+
+    if(!res) SQLite.run(`INSERT INTO profileSystem VALUES ('${msg.author.id}', 200, 0, ${xp}, 0, 0, 0, "Type ${prefix}setinfo to set info", "{}", "{wallSrc: '/walls/p2.png'}"`)
+
+
+    let Image = Canvas.Image,
+    canvas = Canvas.createCanvas(300, 300),
+    ctx = canvas.getContext('2d');
+fs.readFile(__dirname + `/${JSON.parse(res.profileData).wallSrc}`, function (err, Background) {
+  fs.readFile(__dirname + `/walls/p1.png`, function (err, Background) {
+  if (err) return console.log(err);
+  let BG = Canvas.Image;
+  let ground = new Image;
+  ground.src = Background;
+  ctx.drawImage(ground, 0, 0, 297, 305);
+});
+  if (err) return console.log(err);
+  let BG = Canvas.Image;
+  let ground = new Image;
+  ground.src = Background;
+  ctx.drawImage(ground, 0, 0, 300, 305);
+});
+
+
+let url = getvalueof.displayAvatarURL.endsWith(".webp") ? getvalueof.displayAvatarURL.slice(5, -20) + ".png" : getvalueof.displayAvatarURL;
+Jimp.read(url, (err, ava) => {
+    if (err) return console.log(err);
+    ava.getBuffer(Jimp.MIME_PNG, async (err, buf) => {
+        if (err) return console.log(err);
+
+
+        //Avatar
+       let Avatar = Canvas.Image;
+        let ava = new Avatar;
+        ava.src = buf;
+     ctx.drawImage(ava, 8, 43, 80, 85); // احداثيات صورتك
+
+        //ur name
+        ctx.font = 'bold 16px profile'; // حجم الخط و نوعه
+        ctx.fontSize = '40px'; // عرض الخط
+        ctx.fillStyle = "#FFFFFF"; // لون الخط
+        ctx.textAlign = "left"; // محاذا ة النص
+        ctx.fillText(`${getvalueof.username}`, 100, 125) // احداثيات اسمك
+
+         //bord
+         let leaderboard = await SQLite.all(`SELECT * FROM profileSystem ORDER BY xp DESC, credits DESC`);
+        ctx.font = "regular 12px profile" // نوع الخط وحجمه
+        ctx.fontSize = '50px'; // عرض الخط
+        ctx.fillStyle = "#FFFFFF" // لون الخط
+        ctx.textAlign = "left"; // محاذا ة
+        for(var i = 0;i<leaderboard.length;i++) {
+          if(leaderboard[i].id == getvalueof.id) {
+            ctx.fillText(`#${i+1}`, 173, 200)
+          }
+        }
+
+
+        //credit
+        ctx.font = "bold 10px profile" // نوع الخط وحجمه
+        ctx.fontSize = '10px'; // عرض الخط
+        ctx.fillStyle = '#FFFFFF' // لون الخط
+        ctx.textAlign = "left"; // محاذا ة النص
+        ctx.fillText(`$ ${res.credits}`, 156, 163) // احداثيات المصاري
+
+        //poits
+        ctx.font = "bold 13px profile" // ن
+        ctx.fontSize = '10px'; // عرض الخطوع الخط وحجمه
+        ctx.fillStyle = "#FFFFFF" // لون الخط
+        ctx.textAlign = "left"; // محاذا ة النص
+        ctx.fillText(`${res.xp}`, 173, 182) // احداثيات النقاط
+
+        //Level
+        ctx.font = "bold 27px profile" // نوع الخط و حجمه
+        ctx.fontSize = '50px'; // عرض الخط
+        ctx.fillStyle = "#FFFFFF" // لون الخط
+        ctx.textAlign = "left"; // محاذا ة النص
+        ctx.fillText(`${res.level}`, 30, 200) // احداثيات اللفل
+
+        //info
+        ctx.font = "blod 13px profile" // ن
+        ctx.fontSize = '10px'; // عرض الخطوع الخط وحجمه
+        ctx.fillStyle = "#FFFFFF" // لون الخط
+        ctx.textAlign = "left"; // محاذا ة النص
+        ctx.fillText(`${res.info}`, 118, 40) // احداثيات النقاط
+
+        // REP
+        ctx.font = "bold 27px profile";
+        ctx.fontSize = "100px";
+        ctx.fillStyle = "#FFFFFF";
+        ctx.textAlign = "left";
+        ctx.fillText(`+${res.rep}`, 18,270)
+
+msg.channel.send("**:white_check_mark: `Show Profile` ?**" + `${msg.author}`, {
+file: canvas.toBuffer()
+})
+})
+})
+ 
+ 
+};
+ 
+ 
+ 
+ 
+});
+
 client.login(process.env.BOT_TOKEN);
