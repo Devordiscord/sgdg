304911dd1536d42f3ce0a2bfceb218608a9d652f
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-02-08 18:51:31 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -40,9 +40,9 @@ client.on('ready', () => {
 
 
 client.on('guildMemberAdd', member => {
-    wait(1000);
-	
-    const channel = member.guild.channels.find('name', 'chat');
+
+    const channel = member.guild.channels.find('name', '・text');
+  
     const millis = new Date().getTime() - member.user.createdAt.getTime();
     const now = new Date();
     const createdAt = millis / 1000 / 60 / 60 / 24;
@@ -54,7 +54,7 @@ client.on('guildMemberAdd', member => {
     const embed = new Discord.RichEmbed()
     
     .setColor("black")
-    .setDescription(`**${createdAt.toFixed(0)} تاريخ دخولك للدسكورد منذ يوم **`)
+    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
     .setAuthor(member.user.tag, member.user.avatarURL);
     channel.sendEmbed(embed);
 
