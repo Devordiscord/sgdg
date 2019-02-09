1cabd7cab77d13ed0df46d028d73831ef1915143
No Nema <44880953+mrdahom4@users.noreply.github.com>
2019-02-09 10:33:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,19 +88,7 @@ if (message.content.startsWith(adminprefix + 'st')) {
 
 });
 
-client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.find('name', 'join');
-    const millis = new Date().getTime() - member.user.createdAt.getTime();
-    const now = new Date();
-    const createdAt = millis / 1000 / 60 / 60 / 24;
-    const embed = new Discord.RichEmbed()
-    .setColor("black")
-    .setDescription(`**تاريخ دخولك للدسكورد منذ ${createdAt.toFixed(0)} يوم**`)
-    .setAuthor(member.user.tag, member.user.avatarURL);
-     channel.sendEmbed(embed)
 
-  
-});
 
 client.on('ready', () => {
   wait(1200);
