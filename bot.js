6b254758a452a4f1ae1f1dac334309b9f84dda84
Lorans <44880953+mrdahom4@users.noreply.github.com>
2019-02-09 09:26:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1563,45 +1563,47 @@ if(!credits[message.author.id]) credits[message.author.id] = {
 let userData = credits[message.author.id];
 let m = userData.credits;
 
-fs.writeFile("./credits.json", JSON.stringify(credits), (err) => {
+client.on("message", (message) => {
+  let men = message.mentions.users.first()
+ 
+  if (message.author.bot) return;
+    if (message.author.id === client.user.id) return;
+    if(!message.channel.guild) return;
+if (message.content.startsWith(prefix + 'credit')) {
+  if(men) {
+    if (!profile[men.id]) profile[men.id] = {
+    lastDaily:'50',
+    credits: 1,
+  };
+  }
+  fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
     if (err) console.error(err);
   });
-  credits[message.author.id] = {
-      credits: m +0,
-  }
-  
-    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
+  if(men) {
+message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`)
+} else {
+  message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`)
 }
-});
-
-client.on('message', async message => {
-    let amount = 250;
-    if(message.content.startsWith(prefix + "daily")) {
-    if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
-    
-    let userData = credits[message.author.id];
-    let m = userData.credits + amount;
-    credits[message.author.id] = {
-    credits: m
-    };
-
-    fs.writeFile("./credits.json", JSON.stringify(userData.credits + amount), (err) => {
+}
+ 
+if(message.content.startsWith(prefix + "daily")) {
+  if(profile[message.author.id].lastDaily != moment().format('day')) {
+    profile[message.author.id].lastDaily = moment().format('day')
+    profile[message.author.id].credits += 5848548548548548
+     message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: \`\`5848548548548548\`\` credits!**`)
+} else {
+  message.channel.send(`**:stopwatch: | ${men.username} , your daily :yen: credits refreshes ${moment().endOf('day').fromNow()}**`)
+  
+      fs.writeFile("./credits.json", JSON.stringify(userData.credits + amount), (err) => {
     if (err) console.error(err);
     });
-    
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
-        coolDown.add(message.author.id);
-    });
-    
-    setTimeout(() => {
-       coolDown.remove(message.author.id);
-    },86400000);
-    }
-});
+}
+  }
 
-client.on('message', async message => {
+ 
+ let cont = message.content.slice(prefix.length).split(" ");
+let args = cont.slice(1);
+let sender = message.author
 if(message.content.startsWith(prefix + 'trans')) {
           if (!args[0]) {
             message.channel.send(`**Usage: ${prefix}trans @someone amount**`);
